e7a9a32d2f89d2d1b15615dd3e46ebe23543fdf3
Morgan-Sell <morganpsell@gmail.com>
2021-07-12 14:34:46 -0700

    completed geometry game

--- flatmates_bill/main.py
+++ flatmates_bill/main.py
@@ -1,81 +1,27 @@
-from fpdf import FPDF
-import os
-import webbrowser
+from flat import Bill, Flatmate
+from reports import PdfReport, FileSharer
 
-class Bill:
-    """
-    Object that contains data about a bill, such as total amount and period.
-    """
+# CLI App
+amount = float(input("Hey user, enter the bill amount: "))
+period = input("What is the bill period, e.g., December 2020? ")
 
-    def __init__(self, amount, period):
-        self.amount = amount
-        self.period = period
+name1 = input("What is your name? ")
+days_in_house1 = int(input(f"How many days did {name1} stay in the house during the bill period? "))
 
+name2 = input("What is the name of the other flatmate? ")
+days_in_house2 = int(input(f"How many days did {name2} stay in the house during the bill period? "))
 
-class Flatmate:
-    """
-    Creates a person who lives in the flat and pays a share of the bill.
-    """
+the_bill = Bill(amount, period)
+flatmate1 = Flatmate(name1, days_in_house1)
+flatmate2 = Flatmate(name2, days_in_house2)
 
-    def __init__(self, name, days_in_house):
-        self.name = name
-        self.days_in_house = days_in_house
+print(f"{flatmate1.name} pays: ", flatmate1.pays(the_bill, flatmate2))
+print(f"{flatmate2.name} pays: ", flatmate2.pays(the_bill, flatmate1))
 
-    def pays(self, bill, flatmate2):
-        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
-        to_pay = bill.amount * weight
-        return to_pay
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1, flatmate2, the_bill)
 
-class PdfReport:
-    """
-    Create a PDF file that contains data about flatmate such as their names, amount and period.
-    """
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def generate(self, flatmate1, flatmate2, bill):
-
-        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
-        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
-
-        pdf = FPDF(orientation='P', unit='pt', format='A4')
-        pdf.add_page()
-
-        # Add item
-        pdf.image("house.png", w=30, h=30)
-
-        # Insert title
-        pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
-
-        # Insert period label and value
-        pdf.set_font(family="Times", size=14, style='B')
-        pdf.cell(w=100, h=40, txt="Period:", border=0)
-        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
-
-        # Insert name and due amount of first flatmate
-        pdf.set_font(family="Times", size=12)
-        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
-        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
-
-        # Insert name and due amount of second flatmate
-        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
-        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
-
-
-        pdf.output(self.filename)
-
-        webbrowser.open("file://" + os.path.realpath(self.filename))
-
-# test code
-the_bill = Bill(amount = 120, period="April 2021")
-john = Flatmate(name="John", days_in_house=20)
-mary = Flatmate(name="Mary", days_in_house=25)
-
-print("John pays: ", john.pays(bill=the_bill, flatmate2=mary))
-print("Mary pays: ", mary.pays(bill=the_bill, flatmate2=john))
-
-pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=mary, bill=the_bill)
+# Added directory
+file_sharer = FileSharer(filepath=f"files/{pdf_report.filename}")
+print(file_sharer.share())
 
