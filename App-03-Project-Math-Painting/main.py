6c564dd0f83e91b3d11b30fab5051e677e7fb34c
Morgan-Sell <morganpsell@gmail.com>
2021-07-12 22:12:14 -0700

    implemented methods for math painter app

--- App-03-Project-Math-Painting/main.py
+++ App-03-Project-Math-Painting/main.py
@@ -1,31 +1,6 @@
 import numpy as np
 from PIL import Image
 
-class Square:
-
-    def __init__(self, x, y, side, color):
-        self.x = x
-        self.y = y
-        self.side = side
-        self.color = color
-
-    def draw(self, canvas):
-        data = np.zeros((self.side, self.side, 3), dtype=np.uint8)
-        data[:] = self.color
-
-
-class Rectangle:
-
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-
-    def draw(self, canvas):
-        pass
-
 
 class Canvas:
 
@@ -34,9 +9,48 @@ class Canvas:
         self.height = height
         self.color = color
 
-    def make(self, imagepath):
-        data = np.zeros((self.height, self.width, self.color), dtype=np.unit8)
-        data[:] = self.color
+        # create a 3d numpy array of zeros
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        # assign color
+        self.data[:] = color
 
-        img = Image.fromarray(data, 'RGB')
+    def make(self, imagepath):
+        img = Image.fromarray(self.data, 'RGB')
         img.save(imagepath)
+
+
+class Square:
+    """Initiate the shape"""
+    def __init__(self, x, y, side, color):
+        self.x = x
+        self.y = y
+        self.side = side
+        self.color = color
+
+    def draw(self, canvas):
+        """Draw the shape on the canvas."""
+        canvas.data[self.x: self.x+self.side, self.y: self.y+self.side] = self.color
+
+
+class Rectangle:
+    """Initiate the shape"""
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, canvas):
+        """Draw that shape on the canvas."""
+        canvas.data[self.x: self.x + self.height, self.y: self.y + self.width] = self.color
+
+
+
+
+canvas = Canvas(10, 12, [0, 255, 0])
+square = Square(3, 4, 3, [150, 0, 150])
+square.draw(canvas)
+rectangle = Rectangle(8, 4, 8, 2, [0, 0, 200])
+rectangle.draw(canvas)
+canvas.make("test.png")
